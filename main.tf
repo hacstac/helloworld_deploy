7efb6ad2596b088032c4d5fea1416f921e39752a
Akash Rajvanshi <58100882+hacstac@users.noreply.github.com>
2021-07-06 21:30:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,12 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/12images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 
   owners = ["099720109477"]
